fa9b2cc290f9885c426e87a890acd068a537cad3
Agung Tri Atmojo <agung.t.atmojo@gmail.com>
2022-10-26 03:02:15 +0700

    add migrations table

---
+++ database/migrations/2022_10_25_191650_t_sales.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TSales extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode',15);
+            $table->dateTime('tgl');
+            $table->foreignId('cust_id');
+            $table->decimal('subtotal');
+            $table->decimal('diskon');
+            $table->decimal('ongkir');
+            $table->decimal('total_bayar');
+
+            $table->foreign('cust_id')->references('id')->on('m_customer');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_sales');
+    }
+}
